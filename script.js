e1e8e0d4340770b90043c1b583fa85465eb7c941
valladao <valladao@gmail.com>
2021-06-05 11:26:02 -0300

    Initial code release

---
+++ script.js
@@ -0,0 +1,14 @@
+const a = b => b;
+
+const double = [1,2,3].map((num) => num * 2);
+console.log(double); // [2,4,6]
+
+const bob = {
+  _name: "Bob",
+  _friends: ["Sally", "Tom"],
+  printFriends() {
+    this._friends.forEach(f =>
+      console.log(this._name + " knows " + f));
+  }
+};
+console.log(bob.printFriends());
