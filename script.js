910fba25b9fc5055ab456ec3df4923f61cb2c2ed
valladao <valladao@gmail.com>
2021-06-13 14:13:51 -0300

    final commit

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-const a = b => b;
+const a = (b) => b;
 
-const double = [1,2,3].map((num) => num * 2);
+const double = [1, 2, 3].map((num) => num * 2);
 console.log(double); // [2,4,6]
 
 const bob = {
   _name: "Bob",
   _friends: ["Sally", "Tom"],
   printFriends() {
-    this._friends.forEach(f =>
-      console.log(this._name + " knows " + f));
-  }
+    this._friends.forEach((f) => console.log(this._name + " knows " + f));
+  },
 };
 console.log(bob.printFriends());
